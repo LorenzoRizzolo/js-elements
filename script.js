c0267045ef407bc2c8546210085eef100824d9bd
LorenzoRizzolo <122774067+LorenzoRizzolo@users.noreply.github.com>
2023-05-30 07:48:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -38,20 +38,21 @@ var loadingDiv = document.getElementById('loading');
     });
 
 
-   var inputs = document.querySelectorAll(".input-container input");
+var inputs = document.querySelectorAll(".input-container input");
 var placeholders = document.querySelectorAll(".input-container .placeholder");
-
 for (var i = 0; i < inputs.length; i++) {
   inputs[i].addEventListener("focus", function() {
     var placeholder = this.nextElementSibling;
     placeholder.style.top = "-20px";
+    placeholder.style.color = "black";
   });
-  
   inputs[i].addEventListener("blur", function() {
     var placeholder = this.nextElementSibling;
     if (this.value === "") {
       placeholder.style.top = "8px";
       placeholder.style.fontSize = "";
+      placeholder.style.fontSize = "";
+      placeholder.style.color = "#999";
     }
   });
 }

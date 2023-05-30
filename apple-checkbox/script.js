e44781ec90e7b9c231fd9fdd66a01c2f45f0e93c
lorenzorizzolo <lolloriz36@gmail.com>
2023-05-30 22:47:38 +0200

    kjbj

--- apple-checkbox/script.js
+++ apple-checkbox/script.js
@@ -10,3 +10,16 @@ mySwitch.addEventListener("change", function() {
     statusElement.textContent = "OFF";
   }
 });
+
+var switch2 = document.getElementById("choose");
+var status2 = document.getElementById("choose-status");
+if(!switch2.checked){
+  status2.textContent = "Email";
+}
+switch2.addEventListener("change", function() {
+  if(this.checked){
+      status2.textContent = "Phone";
+  }else{
+    status2.textContent = "Email";
+  }
+});

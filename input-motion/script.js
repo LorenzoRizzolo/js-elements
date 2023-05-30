431c1dc64937241be5a66e672699f7f129fa54ed
LorenzoRizzolo <122774067+LorenzoRizzolo@users.noreply.github.com>
2023-05-30 07:25:13 +0200

    Update script.js

--- input-motion/script.js
+++ input-motion/script.js
@@ -1,12 +1,17 @@
-var input = document.getElementById("myInput");
-    var placeholder = document.getElementById("placeholder");
-    input.addEventListener("focus", function() {
-      placeholder.style.top = "-20px";
-      // placeholder.style.fontSize = "100%";
-    });
-    input.addEventListener("blur", function() {
-      if (input.value === "") {
-        placeholder.style.top = "8px";
-        placeholder.style.fontSize = "";
-      }
-});
+var inputs = document.querySelectorAll(".input-container input");
+var placeholders = document.querySelectorAll(".input-container .placeholder");
+
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener("focus", function() {
+    var placeholder = this.nextElementSibling;
+    placeholder.style.top = "-20px";
+  });
+  
+  inputs[i].addEventListener("blur", function() {
+    var placeholder = this.nextElementSibling;
+    if (this.value === "") {
+      placeholder.style.top = "8px";
+      placeholder.style.fontSize = "";
+    }
+  });
+}

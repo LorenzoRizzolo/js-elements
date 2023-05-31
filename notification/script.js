2eed2d05a3d34060f9ff05ad846bd21021634192
lorenzorizzolo <lolloriz36@gmail.com>
2023-05-31 17:31:04 +0200

    ljhb

--- notification/script.js
+++
@@ -1,6 +0,0 @@
-const showNotificationButton = document.getElementById('showNotificationButton');
-const notification = document.getElementById('notification');
-
-showNotificationButton.addEventListener('click', function() {
-  notification.classList.toggle('show');
-});

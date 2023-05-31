bc6e33dd6db3b117792cabc99da1829b2e3161d3
lorenzorizzolo <lolloriz36@gmail.com>
2023-05-31 08:03:48 +0200

    aaple checkbox

--- apple-checkbox/script.js
+++ apple-checkbox/script.js
@@ -1,25 +1,18 @@
-var mySwitch = document.getElementById("mySwitch");
-var statusElement = document.getElementById("status");
-if(!mySwitch.checked){
-    statusElement.textContent = "OFF";
-}
-mySwitch.addEventListener("change", function() {
-  if (this.checked) {
-    statusElement.textContent = "ON";
-  } else {
-    statusElement.textContent = "OFF";
-  }
-});
+var switches = document.querySelectorAll(".mySwitch");
+var statusElements = document.querySelectorAll(".status");
 
-var switch2 = document.getElementById("choose");
-var status2 = document.getElementById("choose-status");
-if(!switch2.checked){
-  status2.textContent = "Email";
-}
-switch2.addEventListener("change", function() {
-  if(this.checked){
-      status2.textContent = "Phone";
-  }else{
-    status2.textContent = "Email";
+switches.forEach(function(switchElement, index) {
+  var statusElement = statusElements[index];
+  if(!switches.checked){
+    statusElement.querySelector("span").textContent = "OFF"
   }
+  switchElement.addEventListener("change", function() {
+    if (this.checked) {
+      statusElement.querySelector("span").textContent = "ON";
+      // Puoi fare altre azioni asincrone qui in base allo stato del pulsante
+    } else {
+      statusElement.querySelector("span").textContent = "OFF";
+      // Puoi fare altre azioni asincrone qui in base allo stato del pulsante
+    }
+  });
 });
